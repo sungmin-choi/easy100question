fa7403a2a9a79f48e812e1a792ec87be3cd90cd7
sungmin-choi <53036141+sungmin-choi@users.noreply.github.com>
2021-01-16 23:17:07 +0800

    ff

--- 1021.py
+++ 1021.py
@@ -1,14 +1,1 @@
-
-N, K = map(int, input().split())
-num = list(input())
-result, dk = [], K
-for i in range(N):
-    while dk > 0 and result:
-        if result[-1] < num[i]:
-            result.pop()
-            dk -= 1
-        else:
-            break
-
-    result.append(num[i])
-print(''.join(result[:N-K]))
+import librosa
